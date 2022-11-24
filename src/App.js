4bbb3f3823a22e91d3872da55ca089f3e016d8ee
NCLAN\dtc <duongthuychuong@gmail.com>
2022-11-24 16:30:22 +0700

    structure the project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from "react";
+import data from "./data";
 function App() {
-  
-  
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Hello");
+  };
+
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className="section-center">
+      <h3>Tired of boring lorem ipsum?</h3>
+      <form onSubmit={handleSubmit} className="lorem-form">
+        <label htmlFor="amount">paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={() => {
+            setCount(count + 1);
+          }}
+        />
+        <button type="submit" className="btn">
+          Generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        <p>
+          Lorem ipsusdfsd sldfjskdlfjksldjf sdfkjsdflkjsdf mxskdfhdkls
+          sdflksdjfksd jf
+        </p>
+        <p>
+          Lorem ipsusdfsd sldfjskdlfjksldjf sdfkjsdflkjsdf mxskdfhdkls
+          sdflksdjfksd jf
+        </p>
+      </article>
+    </section>
+  );
 }
 
 export default App;
